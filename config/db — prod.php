971666171199682b539d72704559b766150d1559
bekir <bekir2912@gmail.com>
2022-02-21 13:15:51 +0500

    First commit

---
+++ config/db — prod.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=u1486707_texnopark_industry',
+    'username' => 'u1486707_texno_admin',
+    'password' => 'Ws8ubnjcaU4y_5j',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
